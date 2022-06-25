6150122d85ba842bea3371809f5e322e98a5510a
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2022-06-24 22:44:19 -0300

    add arquivos iniciais

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import {StatusBar} from 'expo-status-bar';
+import {
+  View, 
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+//Lamps
+import darkLamp from './assets/icons/eco-light-off.png';
+import lightLamp from './assets/icons/eco-light.png';
+//Logos
+import lightLogo from './assets/icons/logo-dio.png';
+import darkLogo from './assets/icons/logo-dio-white.png';
+
+
 
 export default function App() {
+  const toggle = true;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={toggle ? style.lightContainer : style.darkContainer}>
+      <TouchableOpacity onPress={() => {}}>
+        <Image
+          style={toggle ? style.lightOn : style.lightOff}
+          source={toggle ? lightLamp: darkLamp}
+        />
+        <Image
+          style={style.logo}
+          source={toggle ? lightLogo: darkLogo}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const style = StyleSheet.create({
+  darkContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  lightContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  lightOn: {
+    resizeMode: 'contain',
+    alignItems: 'center',
+    width: 250,
+    height: 350,
+  },
+
+  lightOff: {
+    resizeMode: 'contain',
+    alignItems: 'center',
+    width: 250,
+    height: 350,
+    tintColor: 'white',
+  },
+
+  logo: {
+    resizeMode: 'contain',
+    alignItems: 'center',
+    width: 250,
+    height: 150,
   },
 });
---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Flashlight com React Native  :iphone:
+Desafio _DIO_: construindo um _App_ de lanterna usando sensor de movimento com _React Native_.
+
+### Tópicos:
+1. Instalando _libs shake_ e _torch_
+2. Baixando ícones
+3. Estilizar _App_
+4. Ouvir evento nativo de shake
+5. Criar _APK_ para compartilhar _App_
+
+
+> _Digital Innovation One_ : [dio.me](https://www.dio.me/)
+>
+ > _React Native_ : [reactnative.dev](https://reactnative.dev/)
